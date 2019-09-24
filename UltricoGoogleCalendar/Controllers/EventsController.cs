1b712f27bebaaac9bb3d286f778c5dee843fbe0a
Ania <chief209@gmail.com>
2019-09-25 00:14:35 +0200

    base controller for events

---
+++ UltricoGoogleCalendar/Controllers/EventsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UltricoGoogleCalendar.Model;
+using UltricoGoogleCalendar.Services;
+
+namespace UltricoGoogleCalendar.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly IEventService eventService;
+
+        public EventsController(IEventService eventService)
+        {
+            this.eventService = eventService;
+        }
+
+        public OperationResult Create(EventCreateModel model)
+        {
+            eventService.Create(model);
+
+            return OperationResult.OkResult;
+        }
+
+        public OperationResult Update(EventUpdateModel model)
+        {
+            eventService.Update(model);
+
+            return OperationResult.OkResult;
+        }
+    }
+}
