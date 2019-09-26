6297e45d7434e1177b16ecba15f96accfda5ad1e
Ania <chief209@gmail.com>
2019-09-26 21:20:42 +0200

    implemented sending emails

--- UltricoGoogleCalendar/Jobs/EventScheduleJob.cs
+++ UltricoGoogleCalendar/Jobs/EventScheduleJob.cs
@@ -1,12 +1,38 @@
 ﻿using System.Threading.Tasks;
-using UltricoGoogleCalendar.Model;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace UltricoGoogleCalendar.Jobs
 {
     public class EventScheduleJob
     {
-        public async Task Execute(EventResource model)
+        public async Task Execute(EventScheduleArgs jobArgs)
         {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress("from@wp.pl"));
+            email.To.Add(new MailboxAddress(jobArgs.Email));
+            email.Subject = $"Calendar event reminder: {jobArgs.Event.Title}";
+
+            var body = new BodyBuilder
+            {
+                HtmlBody = jobArgs.Event.Description
+            };
+
+            email.Body = body.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Start of provider specific settings
+                await client.ConnectAsync("smtp.host", 587, false).ConfigureAwait(false);
+                await client.AuthenticateAsync("username", "password").ConfigureAwait(false);
+                // End of provider specific settings
+
+                await client.SendAsync(email).ConfigureAwait(false);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
         }
     }
 }
